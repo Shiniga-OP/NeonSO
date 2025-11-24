11a9be2c92568f2d9cc84b965add355152d33eb2
Shiniga-OP <shinigaop@gmail.com>
2025-11-24 08:52:38 -0300

    add comando '@' no NS

---
+++ README.md
@@ -0,0 +1,8 @@
+# Neon
+meu bootloader + kernel feito em assembly ARM64. por que Neon? Sei lá, me pareceu um nome legal kkkkkkk
+
+## drivers:
+1. Terminal UART (QEMU).
+
+## bibliotecas:
+1. Neon Script: uma biblioteca simples de comandos do terminal (ainda vai ser realmente script).
--- biblis/ns.asm
+++ biblis/ns.asm
@@ -1,8 +1,19 @@
-// biblis/neon_script.asm
+// biblis/ns.asm
 .global ns_abrir
 
+msg_coisa: .asciz "alguma coisa\r\n"
+
+.align 2
+1:
+    ldr x0, = msg_coisa
+    bl _escrever_tex
+    b ns_abrir // volta pro loop
+// loop de obter e escrever
 ns_abrir:
     bl _obter_car
+    mov w1, '@'
+    cmp w0, w1 // verifica se digitou "@"
+    b.eq 1b
     bl _escrever_car
     mov w0, 0x0A
     bl _escrever_car
